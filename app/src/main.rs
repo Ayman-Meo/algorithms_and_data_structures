8d2e00f5ae01de23e68bf7dd270d84f389877f24
Ayman Mohammed <eng.ayman.meo@gmail.com>
2026-06-06 01:13:06 +0300

    Add dependances for CLI app, Create packages for remain topics

--- app/src/main.rs
+++ app/src/main.rs
@@ -1,9 +1,5 @@
 
 #![warn(clippy::pedantic)]
-use utilities::log::*;
 fn main() {
     env_logger::init();
-    let l =Logger;
-    l.log(Level::Warn, "Hello, world!");
-    println!("Init: introduction_to_algorithms");
 }
